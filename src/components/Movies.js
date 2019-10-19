1a9fdd05f6e8813f1e468c2c26b7f3bcb4b06a92
Deivi Robles <drozam24@gmail.com>
2019-10-19 17:38:23 -0500

    Define render Structure

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,31 +1,63 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import Style from './styles/Movies.css'
+
 
 export default class Movies extends Component{
 
     state = {
-        movies: []
+        movies: [],
+        genre: [],
+        urlImage: 'http://image.tmdb.org/t/p/w185',
     }
 
     async componentDidMount(){
         const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=58e798bf28ce921eb570c03fb0d70586')
         const data = await res.json()
         this.setState({movies: data.results})
-        console.log(this.state.movies)
+
+        const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=58e798bf28ce921eb570c03fb0d70586')
+        const info = await response.json()
+        this.setState({genre: info.genres})
+        console.log(this.state.genre)
     }
 
     render(){
+        const allMovies = this.state.movies.map(movie =>{
+                return(
+                    <div className="col-6 p-4">
+                        <div className="row cardMovie">
+                            <div className="col-4">
+                                <img src="http://image.tmdb.org/t/p/w185/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg" className="img-fluid"/>
+                            </div>
+                            <div className="col movieData">
+                                <div className="row">
+                                    <div className="col-10">
+                                        <h2>{movie.title}</h2>        
+                                    </div>
+                                    <div className="col text-right movieInfo">
+                                        <p>{movie.vote_average}</p>
+                                    </div>
+                                </div>                                
+                                <span>{movie.release_date} () | {movie.genre_ids}</span>
+                                <p className="pt-4 font-weight-lighter">{movie.overview}</p>
+                                <div className="row pb-4 pt-4">
+                                    <div className="col">
+                                        <button type="button" className="btn btn-outline-info">Show Trailer</button>
+                                    </div>
+                                    <div className="col text-right">
+                                        <p>Add to Favorites &nbsp;<i class="fa fa-heart"></i></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                )
+            })
+        
         return(
-            <div>
-            <p>Movies</p>
-            {
-                this.state.movies.map(movie =>{
-                    return(
-                        <li key={movie.id}>{movie.title}</li>
-                    )
-                })
-            }
-        </div>
+            <div className="row">
+                {allMovies}
+            </div>
         )
     }
 }
