ec32c4e4fc8a8cd1237a4d6d522ce9c12083fb3e
Deivi Robles <drozam24@gmail.com>
2019-10-19 12:20:30 -0500

    Add API with fetch to render data

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Movies extends Component{
+
+    state = {
+        movies: []
+    }
+
+    async componentDidMount(){
+        const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=58e798bf28ce921eb570c03fb0d70586')
+        const data = await res.json()
+        this.setState({movies: data.results})
+        console.log(this.state.movies)
+    }
+
     render(){
         return(
+            <div>
             <p>Movies</p>
+            {
+                this.state.movies.map(movie =>{
+                    return(
+                        <li key={movie.id}>{movie.title}</li>
+                    )
+                })
+            }
+        </div>
         )
     }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ export default class SearchBar extends Component{
         return(
             <div className="input-group mb-3">
   <div className="input-group-prepend">
-    <span className="input-group-text" id="basic-addon1"><i class="fa fa-search"></i></span>
+    <span className="input-group-text" id="basic-addon1"><i className="fa fa-search"></i></span>
   </div>
   <input type="text" className="form-control" placeholder="Search for a movie, series and videos" aria-label="Username" aria-describedby="basic-addon1"/>
 </div>
