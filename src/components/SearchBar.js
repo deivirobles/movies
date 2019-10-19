47a0dbbb88ce80729a1165d786293b08df905356
Deivi Robles <drozam24@gmail.com>
2019-10-19 17:43:17 -0500

    Change layout in SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 
 export default class SearchBar extends Component{
-    render(){
-        return(
-            <div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="basic-addon1"><i className="fa fa-search"></i></span>
-  </div>
-  <input type="text" className="form-control" placeholder="Search for a movie, series and videos" aria-label="Username" aria-describedby="basic-addon1"/>
+render(){
+return(
+<form action="">
+<div className="input-group mb-3">
+	<div className="input-group-prepend">
+		<span className="input-group-text" id="basic-addon1"><i className="fa fa-search"></i></span>
+	</div>
+	<input placeholder="Search for a movie, series and videos" type="text" className="form-control"/>
 </div>
-        )
-    }
+</form>
+)
+}
 }
